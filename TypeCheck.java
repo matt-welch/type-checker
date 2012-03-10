44b9d12f6b27ac2d1169affd67eea351e5c431e6
Matt Welch <mattw0308@gmail.com>
2012-03-10 12:34:21 -0700

    Initial commit to type checker.

---
+++ TypeCheck.java
@@ -0,0 +1,26 @@
+
+import java.util.*;// Scanner
+
+public class TypeCheck {
+
+	public static Scanner keyboard;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// declare local variables
+		
+		// read the program input file in from the command line
+		
+		// parse program file for type declarations of variables
+		// until no more declarations have been found
+
+		// rest of the file is definitions
+		// parse definitions, checking against stored declarations
+		
+		
+	}
+
+}
