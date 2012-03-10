96cf97dee103d056fc622f5ca8894c40fdca7cc1
Matt Welch <matt-welch@users.noreply.github.com>
2012-03-10 13:01:21 -0700

    Reading in a file from argument provided on command line.  Program cannot find default filename file if no argument is provided

--- TypeCheck.java
+++ TypeCheck.java
@@ -1,5 +1,6 @@
 
-import java.util.*;// Scanner
+import java.util.Scanner;// Scanner
+import java.io.*;
 
 public class TypeCheck {
 
@@ -12,14 +13,39 @@ public class TypeCheck {
 
 		// declare local variables
 		
+		
 		// read the program input file in from the command line
+		String filename;
+		if(args.length > 0){
+			filename = args[0];
+		}else{
+			filename = "input.txt";
+		}
+		System.out.printf("Reading in a file from \"%s\":::\n", filename);
+		
+		
+		try
+		{
+			FileInputStream inFile = new FileInputStream(filename);
+			DataInputStream inData = new DataInputStream(inFile);
+			BufferedReader inBuffer = new BufferedReader(new InputStreamReader(inData));
+			String strLine;
+			
+			while((strLine = inBuffer.readLine() ) != null){
+				System.out.println(strLine);
+			}
+			inFile.close();
+		}
+		catch (IOException e){
+			System.err.println("Error: " + e.getMessage());
+		}
+	
 		
 		// parse program file for type declarations of variables
 		// until no more declarations have been found
 
 		// rest of the file is definitions
 		// parse definitions, checking against stored declarations
-		
 		
 	}
 
